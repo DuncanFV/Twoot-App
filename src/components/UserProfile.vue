caf87208f7fc98365e81f0afe5775e93a80d5468
Duncan Ferency-Viars <77799251+DuncanFV@users.noreply.github.com>
2021-03-27 15:20:50 -0400

    Added ability to create new twoots

--- src/components/UserProfile.vue
+++ src/components/UserProfile.vue
@@ -8,6 +8,22 @@
             <div class="user-profile__follower-count">
                 <strong>Followers: </strong> {{ followers }}
             </div>
+            <form class="user-profile__create-twoot" @submit.prevent="createNewTwoot">
+                <label for="newTwoot"><strong>New Twoot</strong></label>
+                <textarea id="newTwoot" rows="4" v-model="newTwootContent"/>
+                <div class="user-profile__create-twoot-type">
+                    <label for="newTwootType"><strong>Type</strong></label>
+                    <select id="newTwootType" v-model="selectedTwootType">
+                        <option :value="option.value" v-for="(option, index) in twootTypes" :key="index">
+                            {{ option.name }}
+                        </option>
+                    </select>
+                </div>
+
+                <button>
+                    Twoot!
+                </button>
+            </form>
         </div>
         <div class="user-profile__twoots-wrapper">
             <TwootItem
@@ -28,22 +44,28 @@ export default {
     name: "UserProfile",
     components: { TwootItem },
     data() {
-    return {
-      followers: 0,
-      user: {
-        id: 1,
-        username: 'DuncanFV',
-        firstName: 'Duncan',
-        lastName: 'Ferency-Viars',
-        email: 'duncanferencyviars@gmail.com',
-        isAdmin: true,
-        twoots: [
-            { id: 1, content: 'Twotter is Amazing!' },
-            { id: 2, content: "Don't forget to check your email!" }
-        ]
-      }
-    }
-  },
+        return {
+            newTwootContent: '',
+            selectedTwootType: 'instant',
+            twootTypes: [
+                { value: 'draft', name: 'Draft' },
+                { value: 'instant', name: 'Instant Twoot' }
+            ],
+            followers: 0,
+            user: {
+                id: 1,
+                username: 'DuncanFV',
+                firstName: 'Duncan',
+                lastName: 'Ferency-Viars',
+                email: 'duncanferencyviars@gmail.com',
+                isAdmin: true,
+                twoots: [
+                    { id: 1, content: 'Twotter is Amazing!' },
+                    { id: 2, content: "Don't forget to check your email!" }
+                ]
+            }
+        }
+    },
   watch: { //allows code to be run when data changes
     followers(newFollowerCount, oldFollowerCount) {
       if (oldFollowerCount < newFollowerCount) {
@@ -62,6 +84,15 @@ export default {
     },
     toggleFavorite(id) {
         console.log(`Favorited Twoot #${id}`)
+    },
+    createNewTwoot() {
+        if (this.newTwootContent && this.selectedTwootType !== 'draft') {
+            this.user.twoots.unshift( {
+                id: this.user.twoots.length + 1,
+                content: this.newTwootContent
+            })
+            this.newTwootContent = '';
+        }
     }
   },
   mounted() { //runs when initialized, or reloaded
@@ -100,5 +131,16 @@ export default {
 
 h1 {
     margin: 0;
+}
+
+.user-profile__twoots-wrapper {
+    display: grid;
+    grid-gap: 10px;
+}
+
+.user-profile__create-twoot {
+    padding-top: 20px;
+    display: flex;
+    flex-direction: column;
 }
 </style>
